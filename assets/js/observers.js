7188176c4cf5bc6d46db98917a8a48dfb3a177c3
adamearnst <84342540+adamearnst@users.noreply.github.com>
2026-03-20 19:06:15 -0400

    Update observers.js

--- assets/js/observers.js
+++ assets/js/observers.js
@@ -1,9 +1,9 @@
-console.log("Updated script loaded!");
+console.log("New script loaded!");
   const nav = document.querySelector("#nav");
   const header = document.querySelector("#header");
   
   const headerOptions = {
-    threshold: 0  // triggers when the full header leaves the viewport
+    rootMargin: "-100px 0px 0px 0px"  // triggers when the full header leaves the viewport
   };
   
   const headerObserver = new IntersectionObserver(function(entries, headerObserver) {
