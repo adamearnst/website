da340b5a28e2a559c208e0d178280124d84404bb
adamearnst <84342540+adamearnst@users.noreply.github.com>
2026-03-21 17:28:22 -0400

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -47,6 +47,30 @@
 		$('.scrolly').scrolly({offset: 88});
 
 	// Nav.
+	fetch('/components/navbar.html')
+  .then(response => response.text())
+  .then(data => {
+    document.getElementById('navbar').innerHTML = data;
+
+    const nav = document.querySelector("#nav");
+    const header = document.querySelector("#header");
+    
+    const headerOptions = {
+      rootMargin: "-100px 0px 0px 0px"  // triggers when the full header leaves the viewport
+    };
+    
+    const headerObserver = new IntersectionObserver(function(entries, headerObserver) {
+      entries.forEach(entry => {
+        if(!entry.isIntersecting) {
+          nav.classList.add("scrolled");
+        } else {
+          nav.classList.remove("scrolled");
+        }
+      });
+    }, headerOptions);
+    
+    headerObserver.observe(header);
+  });
 
 		// Button.
 			$(
