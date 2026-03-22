09a46da20e61968eb3b46e48be905f7bc55989af
adamearnst <84342540+adamearnst@users.noreply.github.com>
2026-03-21 22:38:23 -0400

    Update navbar.js

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,25 +1,20 @@
-fetch('/components/navbar.html')
-  .then(response => response.text())
-  .then(data => {
-    document.getElementById('navbar').innerHTML = data;
 
-    const nav = document.querySelector("#nav");
-    const header = document.querySelector("#header");
-    
-    const headerOptions = {
-      rootMargin: "-100px 0px 0px 0px"  // triggers when the full header leaves the viewport
-    };
-    
-    const headerObserver = new IntersectionObserver(function(entries, headerObserver) {
-      entries.forEach(entry => {
-        if(!entry.isIntersecting) {
-          nav.classList.add("scrolled");
-        } else {
-          nav.classList.remove("scrolled");
-        }
-      });
-    }, headerOptions);
-    
-    headerObserver.observe(header);
+const nav = document.querySelector("#nav");
+const header = document.querySelector("#header");
+
+const headerOptions = {
+  rootMargin: "-100px 0px 0px 0px"  // triggers when the full header leaves the viewport
+};
+
+const headerObserver = new IntersectionObserver(function(entries, headerObserver) {
+  entries.forEach(entry => {
+    if(!entry.isIntersecting) {
+      nav.classList.add("scrolled");
+    } else {
+      nav.classList.remove("scrolled");
+    }
   });
+}, headerOptions);
+
+headerObserver.observe(header);
 
